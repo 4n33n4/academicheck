8aa8bd809ce68bb845f1dc83ed36ea23fe9ecaf3
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-10-06 13:58:39 +0530

    fix: admin.html login.css scripts.js styles.css minor style adjustments in css files add script to show/hide signup link on dropdown menu

--- scripts.js
+++ scripts.js
@@ -2,3 +2,27 @@ function login() {
     const endpoint = document.querySelector("#loginSelect").value + ".html";
     window.open(endpoint, "_self")
 }
+function signup() {
+    window.open("signup.html", "_self")
+}
+
+var loginSelect = document.querySelector("#loginSelect");
+
+
+loginSelect.addEventListener("click", ()=> {
+    var signupLinks = document.getElementsByClassName("signupLinks");
+    for(var i=0; i<signupLinks.length; ++i) {
+        if(loginSelect.value != "admin" ){
+            signupLinks[i].classList.remove("hidden");
+           
+            
+            
+        }
+        else {
+            if( !(signupLinks[i].classList.contains("hidden")) ){
+                signupLinks[i].classList.add("hidden");
+            }
+           }
+        }
+    }
+)
