e0b8294694c1a9c78ae5156303554a702660c03d
7h3cyb3rm0nk <rameesmohd2004@gmail.com>
2023-11-08 01:23:36 +0530

    fix: breaking chanbges

--- scripts.js
+++ scripts.js
@@ -7,10 +7,20 @@ function signup() {
 }
 
 var loginSelect = document.querySelector("#loginSelect");
+var admno = document.querySelector(".studentAdmissionNo");
 
 
 loginSelect.addEventListener("click", ()=> {
     var signupLinks = document.getElementsByClassName("signupLinks");
+    if(loginSelect.value != "student"){
+        admno.classList.add("hidden");
+        
+    }
+    else {
+        if(admno.classList.contains("hidden")){
+            admno.classList.remove("hidden");
+        }
+    }
     for(var i=0; i<signupLinks.length; ++i) {
         if((loginSelect.value != "admin") &&  (loginSelect.value != "teacher") && (loginSelect.value != "student") ){
             signupLinks[i].classList.remove("hidden");
@@ -23,8 +33,10 @@ loginSelect.addEventListener("click", ()=> {
                 signupLinks[i].classList.add("hidden");
             }
            }
+           
+           }
         }
-    }
+    
 )
 
 var signupLinks = document.getElementsByClassName("signupLinks");
@@ -32,5 +44,14 @@ var signupLinks = document.getElementsByClassName("signupLinks");
 if(loginSelect.value == "admin" || loginSelect.value == "teacher") {
     for(var i=0; i<signupLinks.length; i++){
         signupLinks[i].classList.add("hidden");
+    }
+}
+if(loginSelect.value != "student"){
+    admno.classList.add("hidden");
+    
+}
+else {
+    if(admno.classList.contains("hidden")){
+        admno.classList.remove("hidden");
     }
 }
--- student.js
+++ student.js
@@ -5,15 +5,15 @@ var attendance = document.getElementById("attendance");
 var heading  = document.getElementById("heading");
 var defaultText = document.getElementById("default")
 var viewSection = document.getElementById("view");
-
 function getMarks(button) {
 defaultText.classList.add("hidden");
+semValue = button.value;
    
 marks.classList.remove("hidden");
 internalMarks.classList.add("hidden");
 attendance.classList.add("hidden")
 
-heading.innerHTML = "Marks";
+heading.innerHTML = `Marks ${semValue}`;
 
 heading.classList.remove("hidden");
 viewSection.classList.remove("hidden");
@@ -21,24 +21,25 @@ viewSection.classList.remove("hidden");
 
 function getAttendance(button) {
     defaultText.classList.add("hidden");
-       
+    semValue = button.value;
     marks.classList.add("hidden");
     internalMarks.classList.add("hidden");
     attendance.classList.remove("hidden");
     
-    heading.innerHTML = "Attendance";
+    heading.innerHTML = `Attendance ${semValue}`;
     
     heading.classList.remove("hidden");
     }
 
 function getInternals(button) {
     defaultText.classList.add("hidden");
+    semValue = button.value;
        
     marks.classList.add("hidden");
     internalMarks.classList.remove("hidden");
     attendance.classList.add("hidden");
     
-    heading.innerHTML = "Internals";
+    heading.innerHTML = `Internals ${semValue}`;
     
     heading.classList.remove("hidden");
 }
